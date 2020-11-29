ca94658f5ce0872d7c1e16aca9efce93a3708611
Eli Hopkins <ewh3157@rit.edu>
2020-11-29 15:11:18 -0500

    Fixed Syntax of Navigation. Changed Dropdown to be Handled by CSS.

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -1,24 +1,28 @@
 <nav>
     <ul id="outer">
-        <!-- Syntax for this is weird on purpose. Otherwise some weird HTML bug adds whitespace where it shouldn't -->
-        <a href="index.php"><li onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);" class="nondrop" id="first">Homepage
-        </li></a><a href="aboutunix.php"><li onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);" class="nondrop">About UNIX
-        </li></a><a href="navigation.php"><li onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);" class="nondrop">Navigation
-        </li></a><a href="filesystem.php"><li onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);" class="nondrop">File System
-        </li></a><a href="permissions.php"><li onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);" class="nondrop">Permissions
-        </li></a><a href="remotetools.php"><li onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);" class="nondrop">Remote Tools
-        </li></a><li onmouseover="hoverIn(this);dropdownIn(this);" onmouseleave="hoverOut(this);dropdownOut(this)" class="nondrop dropOuter"><a href="tutorials.php">Tutorials</a>
-        <ul class="dropdown"><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a></ul></li><li onmouseover="hoverIn(this);dropdownIn(this);" onmouseleave="hoverOut(this);dropdownOut(this)" class="nondrop dropOuter"><a href="quizzes.php">Quizzes</a>
-        <ul class="dropdown"><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-            </li></a><a href="PLACEHOLDER"><li class="ddli" onmouseover="hoverIn(this);" onmouseleave="hoverOut(this);">PLACEHOLDER
-        </li></a></ul></li>
+        <li class="nondrop" id="first"><a href="index.php">Homepage</a></li>
+        <li class="nondrop"><a href="aboutunix.php">About UNIX</a></li>
+        <li class="nondrop"><a href="navigation.php">Navigation</a></li>
+        <li class="nondrop"><a href="filesystem.php">File System</a></li>
+        <li class="nondrop"><a href="permissions.php">Permissions</a></li>
+        <li class="nondrop"><a href="remotetools.php">Remote Tools</a></li>
+        <li class="nondrop dropOuter"><a href="tutorials.php">Tutorials</a>
+            <ul class="dropdown">
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+            </ul>
+        </li>
+        <li class="nondrop dropOuter"><a href="quizzes.php">Quizzes</a>
+            <ul class="dropdown">
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+                <li class="ddli"><a href="PLACEHOLDER">PLACEHOLDER</a></li>
+            </ul>
+        </li>
     </ul>
 </nav>
