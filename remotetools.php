f09760a37b07193a92da2d0374ab5f39a33b17ef
cad7046 <cad7046@g.rit.edu>
2020-12-02 00:10:39 -0500

    remote tools

--- remotetools.php
+++ remotetools.php
@@ -8,18 +8,13 @@
 ?>
 
 <main>
-    <h1>CONTENT WILL GO HERE</h1>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    <h1>Remote tools</h1>
+    <p>Remote access is the ability to connect to a remote computer or server using a device from any place in the world. This practice is invaluable in transferring data between two points with ease. Through the use of specialized software remote access becomes a simple task.</p>
+    <p>There are many different remote access tools for various operating systems. Many of these are cross-platform as well! A few of the more notable/popular pieces of software include (but are not limited to):</p>
+    <h3><a href = "https://filezilla-project.org/">Filezilla</a></h3>
+    <h3><a href = "https://winscp.net/eng/index.php">WinSCP</a></h3>
+    <h3><a href = "https://panic.com/transmit/">Transmit</a></h3>
+    <p>File transfer tools all come with their unique pro’s and con’s, so when looking for one to use it is important to identify if the software in question will meet your desired needs or not.</p>
     <br />
 </main>
 
