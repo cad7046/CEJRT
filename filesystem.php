1af072d8542d645718415bfe487668707920952e
cad7046 <cad7046@g.rit.edu>
2020-12-01 21:00:38 -0500

    navigation tutorial
    Finished navigation tutorial and added to navigation page - added beginnings of filesystem tutorial on the filesystem page

--- filesystem.php
+++ filesystem.php
@@ -21,6 +21,19 @@
     <br />
     <br />
     <br />
+    
+    
+    <h1>Filesystem Tutorial</h1>
+    <p>Now that you are comfortable with navigating through your files using UNIX, let's make a directory to practice with.</p>
+    <p>From your home directory, navigate to a directory where you want to store your projects. </p>
+    <p>For example, you might want to navigate to your documents folder.</p>
+    <h2>cd documents</h2>
+    <p>Now, you can create the directory using the command </p>
+    <h2>mkdir [directory name]</h2>
+    <p>Now that you have made the directory, you can navigate to that directory using</p>
+    <h2>cd [directory name]</h2>
+    <p>Now you can make files in this directory</p>
+    
 </main>
 
 <?php
